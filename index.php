2e244e0c462d1537e7ff7f4836e84e8d3aab12bc
Halimdv <deville.halim@gmail.com>
2016-06-30 13:51:10 +0200

    add copy and rename

--- index.php
+++ index.php
@@ -13,7 +13,39 @@ fwrite($file, date("Y-m-d H:i:s")." : Le script a été ouvert\r\n");
 fclose($file);
 
 // Fonctions équivalentes
-echo file_get_contents("texte.log");
+$content = file_get_contents("texte.log");
+$content .=  date("Y-m-d H:i:s")." : FPC Le script a été ouvert\r\n";
+file_put_contents("texte.log", $content);
 
+// Pour supprimer un fichier
+// unlink("texte.log");
 
+// Vérifier si un fichier existe ou non
+if(file_exists("texte.log")){
+	echo "Le fichier de log existe";
+}
+
+// Vérifier si un répertoire existe
+if(!is_dir('var')){
+	mkdir('var');
+}
+
+// Modifier les permissions d'un fichier
+chmod("texte.txt", 0777);
+
+// Déplacer ou renommer un fichier
+rename('texte.log', 'var/texte.log');
+
+// Récupérer le nom du fichier courant
+// Récupérer le chemin du fichier courant
+echo __DIR__;
+echo dirname(__FILE__); // Equivalent à echo __DIR__
+echo __FILE__;
+
+// Récupérer la date de dernière modification d'un fichier
+echo date("Y-m-d H-i-s", filemtime("var/texte.log") + 7200 ) ;
+
+// Copier un fichier
+copy("var/texte.log", "var/texte.log.backup");
 ?>
+
