1eeb67ef58268ad729466b7754dcf625f08dc8da
Halimdv <deville.halim@gmail.com>
2016-06-30 17:00:22 +0200

    finishi upload

--- upload.php
+++ upload.php
@@ -7,11 +7,22 @@
 
 if(isset($_FILES['photo'])){
 	var_dump($_FILES);
-	if(!is_dir('upload')){
-		mkdir('upload');
-	}
+	// Je configure ma limite en Ko
+	$limit = 512;
+	// Un tableau avec les extensions autorisées
+	$checkExts = array('jpg', 'jpeg', 'gif','png');
 	$name = uniqid().$_FILES['photo']['name'];
 	$extension = pathinfo($_FILES['photo']['name'])['extension'];
-	move_uploaded_file($_FILES['photo']['tmp_name'], __DIR__."/upload/".$name.".".$extension);
+	// Je convertis la taille du fichier en Ko
+	$size = $_FILES['photo']['size'] /1024;
+	if($size < $limit && in_array($extension, $checkExts)){
+		if(!is_dir('upload')){
+			mkdir('upload');
+		}
+		move_uploaded_file($_FILES['photo']['tmp_name'], __DIR__."/upload/".$name.".".$extension);
+	} else {
+		echo "Votre fichier est trop gros ou le format n'est pas autorisé.";
+	}
+	
 }
 ?>
