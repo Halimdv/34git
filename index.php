ebb8f2b23d0bfc577921b27cab429e211ffc19f1
Halimdv <deville.halim@gmail.com>
2016-06-30 11:52:33 +0200

    add file_get_contents

--- index.php
+++ index.php
@@ -12,4 +12,8 @@ $file = fopen("texte.log", "a");
 fwrite($file, date("Y-m-d H:i:s")." : Le script a été ouvert\r\n");
 fclose($file);
 
+// Fonctions équivalentes
+echo file_get_contents("texte.log");
+
+
 ?>
