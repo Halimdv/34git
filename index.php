21f5f8636c59a325e2f6fba4f28306763438a3a3
Halimdv <deville.halim@gmail.com>
2016-06-30 11:43:43 +0200

    add .gitignore

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 
+// Pour lire un fichier avec fopen, j'ouvre le fichier
 $file = fopen('texte.txt', "r");
+// Avec fread, je peux lire le fichier, je dois lui donner une taille
 var_dump(fread($file, filesize('texte.txt')));
+// Je ferme le fichier
+fclose($file);
+
+// Pour écrire dans un fichier
+$file = fopen("texte.log", "a");
+fwrite($file, date("Y-m-d H:i:s")." : Le script a été ouvert\r\n");
 fclose($file);
 
 ?>
