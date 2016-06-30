28674ac4eabe72073399608ac40163bf71fe3b34
Halimdv <deville.halim@gmail.com>
2016-06-30 16:24:51 +0200

    upload fonctionnalité

--- upload.php
+++ upload.php
@@ -1,3 +1,17 @@
-<form>
-	<input type="file">
+<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="photo">
+	<button name="submit">Envoyer le fichier</button>
 </form>
+
+<?php
+
+if(isset($_FILES['photo'])){
+	var_dump($_FILES);
+	if(!is_dir('upload')){
+		mkdir('upload');
+	}
+	$name = uniqid().$_FILES['photo']['name'];
+	$extension = pathinfo($_FILES['photo']['name'])['extension'];
+	move_uploaded_file($_FILES['photo']['tmp_name'], __DIR__."/upload/".$name.".".$extension);
+}
+?>
